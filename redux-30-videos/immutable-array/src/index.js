25718f36b1f7e4049cd2c1cb09576e9dae513a0c
rcschen <chungshiuan@gmail.com>
2016-09-27 17:39:23 +0800

    update immutable array

--- redux-30-videos/immutable-array/src/index.js
+++ redux-30-videos/immutable-array/src/index.js
@@ -12,6 +12,7 @@ const testAddCounter = () =>{
       expect(addCounter(listBefore)).toEqual(listAfter);
 };
 testAddCounter();
+console.log('all tests passed');
 
 addCounter =(list) =>{
     //return list.concat([0]);
@@ -19,7 +20,6 @@ addCounter =(list) =>{
     
 };
 
-console.log('all tests passed');
 const testAddCounter_with_freeze = () =>{
       const listBefore = [];
       const listAfter = [0];
@@ -29,4 +29,35 @@ const testAddCounter_with_freeze = () =>{
 testAddCounter_with_freeze();
 console.log('pass with freeze')
 
+const removeCounter  =(list, index) =>{
+      //list.splice(index,1);
+      //return list   
+      //return list.slice(0, index).concat(list.slice(index+1));
+      return [...list.slice(0,index),...list.slice(index+1)];
+};
+
+const testRemoveCounter_with_freeze = () =>{
+      const listBefore = [0,10,20];
+      const listAfter = [0,20];
+      deepFreeze(listBefore);
+      expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+};
+testRemoveCounter_with_freeze();
+console.log('pass remove counter with freeze')
+
+
+const incrementCounter=(list,index) =>{
+     //list[index]++;
+     //return list;
+     return list.slice(0,index).concat([ list[index]+1 ]).concat(list.slice(index+1));
+     //return [...list.slice(0,index), list[index]+1, ...list.slice(index+1)];
+};
+const testIncrementCounter_with_freeze = ()=>{
+      const listBefore = [0,10,20];
+      const listAfter=[0,11,20];
+      deepFreeze(listBefore);
+      expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+};
+testIncrementCounter_with_freeze();
+console.log('pass increment counter with freeze')
 
